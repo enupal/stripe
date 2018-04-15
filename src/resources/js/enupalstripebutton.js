e564f641ef47029441a343f693390b93ff39e259
Andre L <andrelopezd@gmail.com>
2018-04-15 11:50:56 -0400

    Updates stripabutton js

--- src/resources/js/enupalstripebutton.js
+++ src/resources/js/enupalstripebutton.js
@@ -5,46 +5,19 @@
     var EnupalStripe = Garnish.Base.extend({
 
         options: null,
-        $sizeSelect: null,
-        $languageSelect: null,
         $unlimitedStock: null,
+        $currencySelect: null,
 
         /**
          * The constructor.
          */
         init: function() {
             // init method
-            this.$sizeSelect = $("#fields-size");
-            this.$languageSelect = $("#fields-language");
             this.$unlimitedStock = $("#fields-unlimited-stock");
             this.$currencySelect = $("#fields-currency");
 
-            this.changeOptions();
-            this.addListener(this.$sizeSelect, 'change', 'changeOptions');
-            this.addListener(this.$languageSelect, 'change', 'changeOptions');
             this.addListener(this.$unlimitedStock, 'change', 'handleUnlimitedStock');
             this.addListener(this.$currencySelect, 'change', 'handleCurrencySelect');
-        },
-
-        changeOptions: function()
-        {
-            var value = this.$sizeSelect.val();
-            var language = this.$languageSelect.val();
-            var data = {'size': value, 'language': language};
-
-            Craft.postActionRequest('enupal-stripe/settings/get-size-url', data, $.proxy(function(response, textStatus)
-            {
-                var statusSuccess = (textStatus === 'success');
-
-                if(statusSuccess && response.buttonUrl)
-                {
-                    $("#fields-button-preview").attr("src",response.buttonUrl);
-                }
-                else
-                {
-                    Craft.cp.displayError(Craft.t('enupal-stripe','An unknown error occurred.'));
-                }
-            }, this));
         },
 
         handleUnlimitedStock: function(option) {
