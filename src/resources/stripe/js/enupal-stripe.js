8af8f9057b31d2d1c5fcdfb91e009ad44fc6b35c
Andre L <andrelopezd@gmail.com>
2018-04-17 10:28:28 -0400

    Adds addValuesToForm function

--- src/resources/stripe/js/enupal-stripe.js
+++ src/resources/stripe/js/enupal-stripe.js
@@ -10,13 +10,13 @@ var enupalStripe = {};
         init: function() {
             this.buttonsList = $('.enupal-stripe-form');
 
-            this.buttonsList.each( function() {
-                var enupalButtonElement = $( this );
-                enupalStripe.initializeForm( enupalButtonElement );
-            } );
+            this.buttonsList.each(function() {
+                var enupalButtonElement = $(this);
+                enupalStripe.initializeForm(enupalButtonElement);
+            });
         },
 
-        initializeForm: function( enupalButtonElement ) {
+        initializeForm: function(enupalButtonElement) {
             // get the form ID
             var enupalStripeData = $.parseJSON($(enupalButtonElement).find('[name="enupalStripeData"]').val());
 
@@ -27,58 +27,85 @@ var enupalStripe = {};
 
             // Stripe Checkout handler configuration.
             // Docs: https://stripe.com/docs/checkout#integration-custom
-            stripeHandler = StripeCheckout.configure( {
+            stripeHandler = StripeCheckout.configure({
                 key: enupalStripeData.pbk,
                 token: processStripeToken,
                 opened: function() {
                 },
                 closed: function() {
                 }
-            } );
+            });
 
             // Callback function to handle StripeCheckout.configure
-            function processStripeToken( token, args ) {
+            function processStripeToken(token, args) {
                 // At this point the Stripe Checkout overlay is validated and submitted.
                 // Set values to hidden elements to pass via POST when submitting the form for payment.
-                enupalButtonElement.find( '.enupal-stripe-field-token' ).val( token.id );
-                enupalButtonElement.find( '.enupal-stripe-field-email' ).val( token.email );
+                enupalButtonElement.find('.enupal-stripe-field-token').val(token.id);
+                enupalButtonElement.find('.enupal-stripe-field-email').val(token.email);
 
                 // Handle args
-                enupalStripe.handleStripeArgs( enupalButtonElement, args );
+                enupalStripe.addValuesToForm(enupalButtonElement, args);
 
                 // Disable pay button and show a nice UI message
-                enupalButtonElement.find( '.enupal-stripe-button' )
-                    .prop( 'disabled', true )
-                    .find( 'span' )
-                    .text( enupalStripeData.paymentButtonProcessingText );
+                enupalButtonElement.find('.enupal-stripe-button')
+                    .prop('disabled', true)
+                    .find('span')
+                    .text(enupalStripeData.paymentButtonProcessingText);
 
                 // Unbind original form submit trigger before calling again to "reset" it and submit normally.
-                enupalButtonElement.unbind( 'submit', [ enupalButtonElement, enupalStripeData ] );
+                enupalButtonElement.unbind('submit', [enupalButtonElement, enupalStripeData]);
 
                 enupalButtonElement.submit();
             }
 
             // Pay Button clicked
-            enupalButtonElement.find( '.enupal-stripe-button' ).on( 'click', function( e ) {
+            enupalButtonElement.find('.enupal-stripe-button').on('click', function(e) {
                 e.preventDefault();
 
-                enupalStripe.submitPayment( enupalButtonElement, enupalStripeData, stripeHandler );
-            } );
+                enupalStripe.submitPayment(enupalButtonElement, enupalStripeData, stripeHandler);
+            });
         },
 
-        submitPayment: function( enupalButtonElement, enupalStripeData, stripeHandler ) {
-            var stripeConfig =  enupalStripeData.stripe;
+        addValuesToForm: function(enupalButtonElement, args) {
+
+            if (args.shipping_name) {
+                spFormElem.find('.enupal-stripe-field-name').val(args.shipping_name);
+            }
+
+            if (args.shipping_address_country) {
+                spFormElem.find('.enupal-stripe-field-country').val(args.shipping_address_country);
+            }
+
+            if (args.shipping_address_zip) {
+                spFormElem.find('.enupal-stripe-field-zip').val(args.shipping_address_zip);
+            }
+
+            if (args.shipping_address_state) {
+                spFormElem.find('.enupal-stripe-field-state').val(args.shipping_address_state);
+            }
+
+            if (args.shipping_address_line1) {
+                spFormElem.find('.enupal-stripe-field-address-line1').val(args.shipping_address_line1);
+            }
+
+            if (args.shipping_address_city) {
+                spFormElem.find('.enupal-stripe-field-city').val(args.shipping_address_city);
+            }
+        },
+
+        submitPayment: function(enupalButtonElement, enupalStripeData, stripeHandler) {
+            var stripeConfig = enupalStripeData.stripe;
             stripeConfig.amount = this.convertToCents(stripeConfig.amount);
             // If everything checks out then let's open the form
             stripeHandler.open(stripeConfig);
         },
 
-        convertToCents: function( amount ) {
-            return ( amount * 100 );
+        convertToCents: function(amount) {
+            return (amount * 100);
         }
     };
 
-    $( document ).ready( function( $ ) {
+    $(document).ready(function($) {
         enupalStripe.init();
-    } );
+    });
 })(jQuery);
