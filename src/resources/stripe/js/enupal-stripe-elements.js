6713095a85504c1214156a8d30c90f0a495af74c
Andre L <andrelopezd@gmail.com>
2018-07-26 12:56:48 -0400

    Adds Ideal initial display function. #5

--- src/resources/stripe/js/enupal-stripe-elements.js
+++ src/resources/stripe/js/enupal-stripe-elements.js
@@ -65,6 +65,14 @@ var enupalStripe = {};
                 }
             };
 
+            //this.createCardElement(stripe, enupalStripeData, elements, style);
+            this.createIdealElement(stripe, enupalStripeData, enupalButtonElement, elements, style);
+
+
+
+        },
+
+        createCardElement: function(stripe, enupalStripeData, elements, style) {
             // Create an instance of the card Element.
             var card = elements.create('card', {style: style});
 
@@ -97,6 +105,164 @@ var enupalStripe = {};
                     }
                 });
             });
+        },
+
+        createIdealElement: function(stripe, enupalStripeData, enupalButtonElement, elements, style) {
+
+            var that = this;
+
+            // Create an instance of the idealBank Element.
+            var idealBank = elements.create('idealBank', {style: style});
+
+            // Add an instance of the idealBank Element into the `ideal-bank-element` <div>.
+            idealBank.mount('#ideal-bank-element-'+enupalStripeData.paymentFormId);
+
+            var errorMessage = document.getElementById('error-message-'+enupalStripeData.paymentFormId);
+
+            // Handle form submission.
+            enupalButtonElement[0].addEventListener('submit', function(event) {
+                event.preventDefault();
+                //showLoading();
+
+                var enupalStripeDataSubmission = $.extend(true,{},enupalStripeData);
+                var stripeConfig = enupalStripeDataSubmission.stripe;
+                stripeConfig.amount = that.convertToCents(that.getFinalAmount(enupalButtonElement, enupalStripeDataSubmission), stripeConfig.currency);
+                enupalButtonElement.find('[name="enupalStripe[amount]"]').val(stripeConfig.amount);
+                enupalButtonElement.find('[name="enupalStripe[testMode]"]').val(enupalStripeDataSubmission.testMode);
+                console.log(enupalButtonElement.find('[name="name"]').val());
+
+                var sourceData = {
+                    type: 'ideal',
+                    amount: stripeConfig.amount,
+                    currency: 'eur',
+                    owner: {
+                        name: enupalButtonElement.find('[name="name"]').val(),
+                    },
+                    // Specify the URL to which the customer should be redirected
+                    // after paying.
+                    redirect: {
+                        return_url: 'https://shop.example.com/crtA6B28E1',
+                    },
+                };
+
+                // Call `stripe.createSource` with the idealBank Element and additional options.
+                stripe.createSource(idealBank, sourceData).then(function(result) {
+                    if (result.error) {
+                        // Inform the customer that there was an error.
+                        errorMessage.textContent = result.error.message;
+                        errorMessage.classList.add('visible');
+                        stopLoading();
+                    } else {
+                        // Redirect the customer to the authorization URL.
+                        errorMessage.classList.remove('visible');
+                        console.log(result.source)
+                        //stripeSourceHandler(result.source);
+                    }
+                });
+            });
+        },
+
+        getFinalAmount: function(enupalButtonElement, enupalStripeData){
+            // We always return a default amount
+            var finalAmount = enupalStripeData.stripe.amount;
+            var fee = 0;
+            var isRecurring = false;
+
+            if (!enupalStripeData.enableSubscriptions){
+                // Check if custom amount
+                if ( enupalStripeData.amountType == 1) {
+                    var customAmount = enupalButtonElement.find( '[name="enupalStripe[customAmount]"]' ).val();
+                    isRecurring = enupalButtonElement.find( '[name="enupalStripe[recurringToggle]"]' ).is(":checked");
+
+                    if ( ( 'undefined' !== customAmount ) && ( customAmount > 0 ) ) {
+                        finalAmount = customAmount;
+                    }
+                }
+            }else{
+                // Subscriptions!
+                var subscriptionType = enupalStripeData.subscriptionType;
+
+                if (subscriptionType == 0){
+
+                    if (enupalStripeData.singleSetupFee > 0){
+                        fee = enupalStripeData.singleSetupFee;
+                    }
+                    // single plan
+                    if (enupalStripeData.enableCustomPlanAmount){
+                        // Custom plan
+                        var customPlanAmount = enupalButtonElement.find( '[name="enupalStripe[customPlanAmount]"]' ).val();
+
+                        if ( ( 'undefined' !== customPlanAmount ) && ( customPlanAmount > 0 ) ) {
+                            finalAmount = customPlanAmount;
+                        }
+                    }
+                }else{
+                    // Custom plan
+                    var customPlanAmountId = null;
+                    if (enupalStripeData.subscriptionStyle == 'radio'){
+                        customPlanAmountId = $('input[name="enupalStripe[enupalMultiPlan]"]:checked').val();
+                    }else{
+                        customPlanAmountId = enupalButtonElement.find( '[name="enupalStripe[enupalMultiPlan]"]' ).val();
+                    }
+                    var customPlanAmount = null;
+
+                    if (customPlanAmountId in enupalStripeData.multiplePlansAmounts){
+                        customPlanAmount = enupalStripeData.multiplePlansAmounts[customPlanAmountId]['amount'];
+                        enupalStripeData.stripe['currency'] =  enupalStripeData.multiplePlansAmounts[customPlanAmountId]['currency'];
+                    }
+
+                    if (customPlanAmountId in enupalStripeData.setupFees){
+                        var multiplePlanFee = enupalStripeData.setupFees[customPlanAmountId];
+
+                        if (multiplePlanFee > 0){
+                            fee = multiplePlanFee;
+                        }
+                    }
+
+                    // Multi-select plan
+                    if ( ( 'undefined' !== customPlanAmount ) && ( customPlanAmount > 0 ) ) {
+                        finalAmount = customPlanAmount;
+                    }
+                }
+            }
+
+            if ((enupalStripeData.applyTax || isRecurring) && enupalStripeData.enableTaxes){
+                var tax = parseFloat((enupalStripeData.tax / 100) * (parseFloat(finalAmount) + parseFloat(fee))).toFixed(2);
+                finalAmount = parseFloat(finalAmount) + parseFloat(tax);
+                var taxLabel = enupalStripeData.taxLabel + ': '+enupalStripeData.currencySymbol+tax;
+
+                enupalButtonElement.find('[name="enupalStripe[taxAmount]"]').val(tax);
+                enupalButtonElement.find( '[name="tax-amount-label"]' ).empty().append(taxLabel);
+            }
+
+            return parseFloat(finalAmount) + parseFloat(fee);
+        },
+
+        convertToCents: function(amount, currency) {
+            if (this.hasZeroDecimals(currency)){
+                return amount;
+            }
+
+            return (amount * 100).toFixed(0);
+        },
+
+        convertFromCents: function(amount, currency) {
+            if (this.hasZeroDecimals(currency)){
+                return amount;
+            }
+
+            return (amount / 100);
+        },
+
+        hasZeroDecimals: function(currency){
+            // Adds support for currencies with zero decimals
+            for (var i = 0; i < this.zeroDecimals.length; i++) {
+                if (this.zeroDecimals[i] === currency.toUpperCase()){
+                    return true;
+                }
+            }
+
+            return false;
         }
     };
 
