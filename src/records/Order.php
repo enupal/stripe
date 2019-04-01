fe4ea8e6a5cd05d2ce43e2be0a69a711e7b71104
Andre L <andrelopezd@gmail.com>
2019-04-01 10:26:50 -0400

    Removes unused address columns

--- src/records/Order.php
+++ src/records/Order.php
@@ -26,18 +26,9 @@ use craft\records\Element;
  * @property $stripeTransactionId
  * @property $email
  * @property $isCompleted
- * @property $firstName
- * @property $lastName
  * @property $shipping
  * @property $tax
  * @property $discount
- * @property $addressCity
- * @property $addressCountry
- * @property $addressState
- * @property $addressCountryCode
- * @property $addressName
- * @property $addressStreet
- * @property $addressZip
  * @property $variants
  * @property $transactionInfo
  * @property $testMode
