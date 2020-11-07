2334ca537c4dc953ed602282c8d0b3b199e42a87
Andre <andrelopezd@gmail.com>
2020-11-07 13:30:30 +0000

    Adds support for new payment methods new stripe checkout

--- src/enums/CheckoutPaymentType.php
+++ src/enums/CheckoutPaymentType.php
@@ -16,6 +16,14 @@ abstract class CheckoutPaymentType extends BaseEnum
     // Constants
     // =========================================================================
     const CC = 'card';
-    const IDEAL  = 'ideal';
-    const FPX  = 'fpx';
+    const IDEAL = 'ideal';
+    const FPX = 'fpx';
+    const ALIPAY = 'alipay';
+    const BACS_DEBIT = 'bacs_debit';
+    const BANCONTACT = 'bancontact';
+    const GIROPAY = 'giropay';
+    const P24 = 'p24';
+    const EPS = 'eps';
+    const SOFORT = 'sofort';
+    const SEPA_DEBIT = 'sepa_debit';
 }
